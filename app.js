724fbc3914f0ac59c9c7ff3a2e60170f8fe08fd7
sp <sp@outsi.de>
2022-06-29 11:06:57 +0300

    questions route

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(cors());
 
-// questions.routes(app);
+questions.routes(app);
 
 // mongoose.Promise = global.Promise;
 
